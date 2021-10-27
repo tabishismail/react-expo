16ee39c539f4acf6d70857c5a17d5d3c52f1b65f
tabish1235 <tabish554@gmail.com>
2021-10-28 01:02:55 +0500

    packages install

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Login, NewPassword, Reset, SignUp } from './Screens';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="Reset" component={Reset} />
+          <Stack.Screen name="NewPassword" component={NewPassword} />
+        </Stack.Navigator>
+      </NavigationContainer>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,6 +27,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width:"100%",
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
